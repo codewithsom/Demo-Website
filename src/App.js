36754f996009724a16f084c56191c098d78b85c5
7VILLAIN <78035544+7VILLAIN@users.noreply.github.com>
2022-11-22 19:11:05 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Analytics from './components/Analytics';
+import Cards from './components/Cards';
+import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import Newsletter from './components/Newsletter';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Navbar />
       <Hero />
       <Analytics />
+      <Newsletter />
+      <Cards />
+      <Footer />
     </div>
   );
 }
