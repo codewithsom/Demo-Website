2ef5a994f49f68f4175ce08f43c308e4ac7b1446
7VILLAIN <78035544+7VILLAIN@users.noreply.github.com>
2022-11-22 17:14:50 +0530

    day

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Analytics from './components/Analytics';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Analytics />
+    </div>
+  );
+}
+
+export default App;
